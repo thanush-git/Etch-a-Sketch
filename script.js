6fe6d5586df772ab1484606a6ed6c7f9dad34278
thanush <thanush.learn@gmail.com>
2024-11-14 23:29:55 +0530

    functional dynamic pixels, bassic event listener implemented

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const defaultPixel = 16; // Default pixel size when page loads
 pixelContainer.style.height = "80vh"
 pixelContainer.style.width = "80vh"
 pixelContainer.style.border = "1px black solid"
-
+pixelContainer.style.display = "flex"
+pixelContainer.style.flexWrap = "wrap"
 
 function clearContainer (x) {
     x.innerHTML = ''
@@ -23,9 +24,14 @@ function populatePixels(num) {
     pixel.forEach(function (pixel) {
         pixel.style.flex = `1 1 calc(100% / ${num})`;
         pixel.style.height = `calc(100% / ${num})`;
+        if (num>50){
+            pixel.style.border = '0.001em solid #D3D3D3';
+        }
       });
+      
 }
 
+populatePixels(defaultPixel);
 
 pixelPrompt.addEventListener("click", function () {
     let numPixels = parseInt(prompt("Enter the number of pixels between 1 to 100"));
@@ -33,4 +39,9 @@ pixelPrompt.addEventListener("click", function () {
     populatePixels(numPixels);
 });
 
-
+const pixels = document.querySelectorAll('.container div')
+pixels.forEach (function(color){
+    color.addEventListener("mouseover", function(e) {
+        color.style.backgroundColor = 'black';
+    });
+});
